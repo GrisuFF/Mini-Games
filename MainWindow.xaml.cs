0771ef6d08df791bcd4169863e32e346c3458f60
Stefan Frandsen <stefan@fam-frandsen.ch>
2022-02-16 13:41:22 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mini_Games
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnTetris_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Das Spiel befindet sich in Entwicklung!", "Tetris", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnTicTacToe_Click(object sender, RoutedEventArgs e)
+        {
+            TicTacToe_Start wnd = new TicTacToe_Start();
+            wnd.ShowDialog();
+        }
+    }
+}
---
+++ TicTacToe Start.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mini_Games
+{
+    /// <summary>
+    /// Interaktionslogik für TicTacToe_Start.xaml
+    /// </summary>
+    public partial class TicTacToe_Start : Window
+    {
+        public TicTacToe_Start()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartTicTacToe_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnCancelTicTacToe_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tbNamePlayer1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbNamePlayer1.Text == "" && tbNamePlayer2.Text == "")
+            {
+                btnStartTicTacToe.IsEnabled = false;
+            }
+            else
+            {
+                btnStartTicTacToe.IsEnabled = true;
+            }
+        }
+    }
+}
